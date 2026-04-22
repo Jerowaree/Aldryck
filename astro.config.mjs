c11f8dc5a31632494a4a2180f3db8e923bffcc73
Jerowaree <dqlizano18@gmail.com>
2026-04-21 23:34:08 -0500

    servir en vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,13 @@ import { defineConfig } from "astro/config";
 import node from "@astrojs/node";
 import react from "@astrojs/react";
 import tailwindcss from "@tailwindcss/vite";
+import vercel from '@astrojs/vercel';
 
 export default defineConfig({
-  output: "server",
-  adapter: node({ mode: "standalone" }),
+  output: "static",
+  adapter: vercel({
+    edge: true,
+  }),
   integrations: [react()],
   vite: {
     plugins: [tailwindcss()],
